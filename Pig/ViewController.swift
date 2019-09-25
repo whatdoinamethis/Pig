ad20a9ae46e425ad9520fbe300d4efd31b2221af
Colin Wunch <colin.wunch.477@k12.friscoisd.org>
2019-09-25 09:32:52 -0500

    Added the random number function for the dice roll, and also added a temporary score related to it.

--- Pig/ViewController.swift
+++ Pig/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var rollButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var pigOut: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func rollPress(_ sender: Any) {
+        var tempScore: Int = 0
+        let roll = Int.random(in: 1...6)
+        tempScore+=roll
+        scoreLabel.text = "Your score \(tempScore)"
+        if(roll==1) {
+        tempScore = 0
+        pigOut.isHidden = false
+        rollButton.isHidden = true
+        }
+    }
+    
 }
 
